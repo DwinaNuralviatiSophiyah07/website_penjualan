53a4b313123ac900b8e8d80e853a35bfa54867a1
DwinaNuralviatiSophiyah07 <nuralviatid@gmail.com>
2022-01-10 15:47:53 +0700

    uas sibw

---
+++ resources/views/mt_shop/index.blade.php
@@ -0,0 +1,247 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Mt.shop</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <header>
+            <h1 class="site-heading text-center text-faded d-none d-lg-block">
+                <span class="site-heading-upper text-primary mb-3">Kalau Lapar Jajan Disini</span>
+                <span class="site-heading-lower text-primary mb-3">MT.Shop</span>               
+            </h1>
+        </header>
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"><img src="assets/img/hh.png" width="50" height="45"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="#home">Home</a></li>
+                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="#about">About</a></li>
+                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="#products">Products</a></li>
+                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="#store">Store</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <section class="page-section clearfix" id="home">
+            <div class="container">
+                <div class="intro">
+                    <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="assets/img/home.png" alt="..."/>
+                    <div class="intro-text left-0 text-center bg-faded p-5 rounded">
+                        <h2 class="section-heading mb-4">
+                            <span class="section-heading-upper">Rasakan Kelezatannya</span>
+                            <span class="section-heading-lower">Tanpa Takut Gemuk</span>
+                        </h2>
+                        <p class="mb-3">
+                            Menonton film atau mengerjakan tugas kadang kurang lengkap jika tidak ada camilannya. 
+                            Disini kami menyediakan beberapa camilan dengan beberapa variasi . Dan tekstur yaang crunchy dan rasa gurihnya saat dikunyah dijamin membuat Anda ketagihan, apalagi harganya sangat
+                            bersahabat.</p>
+                        <div class="intro-button mx-auto"><a class="btn btn-primary btn-xl" href="https://api.whatsapp.com/send?6282128391139&text=Saya%20tertarik%20dengan%20produk%20dari%20MT.Shop">Beli Sekarang!</a></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <!-- About-->
+        <section class="page-section about-heading" id = "about">
+            <div class="container">
+                <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="assets/img/profil.png" alt="..." width="1000" height="1000"/>
+                <div class="about-heading-content">
+                    <div class="row">
+                        <div class="col-xl-9 col-lg-10 mx-auto">
+                            <div class="bg-faded rounded p-5">
+                                <h2 class="section-heading mb-4">
+                                    <span class="section-heading-lower">Profil MT.Shop</span>
+                                </h2>
+                                <p> MT.Shop adalah sebuah industri UMKM produk 
+                                    makanan ringan berupa camilan UMKM ini didirikan pada tahun 2021. Produk industri ini berupa camilan yang dapat digunakan untuk 
+                                    menemani kita dalam melakukan sebuah kegiatan, selain itu camilan juga 
+                                    dikemas dengan simpel, sehingga memudahkan konsumen untuk membawa 
+                                    produk tersebut saat berpergian. Walaupun tergolong UMKM baru, akan 
+                                    tetapi MT.Shop ini telah memiliki reseller di beberapa daerah.</p>                               
+                                <p class="mb-0">
+                                    Visi MT.Shop :
+                                    Menjadi UMKM Nasional produk makanan ringan berupa camilan 
+                                    yang berkualitas serta menjamin kepuasan pelanggan dengan menebar 
+                                    cinta disetiap kriuknya. 
+                                </p>  
+                                <p class="mb-0">
+                                   Misi MT.Shop
+                                </p>
+                                <p class="mb-0">
+                                1. Produktifitas terjaga dan unggul dalam mutu 
+                                </p>
+                                <p class="mb-0">
+                                2. Menjamin keamanan produk  
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Produk -->
+        <section class="page-section" id = "products">
+        <div class="container">
+        <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="assets/img/produk2.png" alt="..." />
+        <div class="product-item">
+                    <div class="product-item-title d-flex">
+                        <div class="bg-faded p-5 d-flex ms-auto rounded">
+                            <h2 class="section-heading mb-0">
+                                <span class="section-heading-upper">Products Best Seller MT.Shop</span>
+                                <span class="section-heading-lower">Makaroni Bantet</span>
+                            </h2>
+                        </div>
+                    </div>
+                    <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="assets/img/makaroni.jpg" alt="..." width="800" height="800"/>
+                    <div class="product-item-description d-flex me-auto"><br><br>
+                        <div class="bg-faded p-5 rounded"><p class="mb-0">Makaroni Pedas Gurih Poll. Dengan tekstur crunchy dan rasa gurihnya saat dikunyah dijamin membuat Anda ketagihan, apalagi harganya sangat bersahabat. Terdapat 3 variasi rasa, asin , pedas dan extra pedas.</p></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="page-section">
+            <div class="container">
+                <div class="product-item">
+                    <div class="product-item-title d-flex">
+                        <div class="bg-faded p-5 d-flex me-auto rounded">
+                            <h2 class="section-heading mb-0">
+                                <span class="section-heading-upper">Produk yang bikin Ketagihan</span>
+                                <span class="section-heading-lower">Kripik Kaca</span>
+                            </h2>
+                        </div>
+                    </div>
+                    <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="assets/img/kripikkaca.jpg" alt="..." width="800" height="800"/>
+                    <div class="product-item-description d-flex ms-auto">
+                        <div class="bg-faded p-5 rounded"><p class="mb-0">Keripik yang dibuat tipis layaknya kaca, begitu renyah saat di konsumsi, sehingga membuat ketagihan bagi para penggemar cemilan pedas. Kripca dengan 4 varian rasa : 
+                        <p> 1. Original </p>
+                        <p> 2. Pedas </p> 
+                        <p> 3. Balado </p> 
+                        <p> 4. Ekstra Daun jeruk</p>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="page-section">
+            <div class="container">
+                <div class="product-item">
+                    <div class="product-item-title d-flex">
+                        <div class="bg-faded p-5 d-flex ms-auto rounded">
+                            <h2 class="section-heading mb-0">
+                                <span class="section-heading-upper">Produk rumahan yang berkualitas</span>
+                                <span class="section-heading-lower">Lumpia Kriuk</span>
+                            </h2>
+                        </div>
+                    </div>
+                    <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="assets/img/lumpia.jpg" alt="..." width="800" height="800" />
+                    <div class="product-item-description d-flex me-auto">
+                        <div class="bg-faded p-5 rounded"><p class="mb-0">Buruan cobain renyah dan kriuknya keripik pangsit buatan kami, dengan cita rasa yang sangat gurih. Cocok dimakan dengan nasi hangat atau teman ngemil. Tersedia dalam  varian rasa : balado, sapi panggang, keju asin, jagung bakar, pedas asin.</p></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Store--> 
+        <section class="page-section cta" id = "store">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-9 mx-auto">
+                        <div class="cta-inner bg-faded text-center rounded">
+                            <h2 class="section-heading mb-5">
+                                <span class="section-heading-upper">Come On In</span>
+                                <span class="section-heading-lower">We're Open</span>
+                            </h2>
+                            <ul class="list-unstyled list-hours mb-5 text-left mx-auto">
+                                <li class="list-unstyled-item list-hours-item d-flex">
+                                    Sunday
+                                    <span class="ms-auto">Closed</span>
+                                </li>
+                                <li class="list-unstyled-item list-hours-item d-flex">
+                                    Monday
+                                    <span class="ms-auto">7:00 AM to 8:00 PM</span>
+                                </li>
+                                <li class="list-unstyled-item list-hours-item d-flex">
+                                    Tuesday
+                                    <span class="ms-auto">7:00 AM to 8:00 PM</span>
+                                </li>
+                                <li class="list-unstyled-item list-hours-item d-flex">
+                                    Wednesday
+                                    <span class="ms-auto">7:00 AM to 8:00 PM</span>
+                                </li>
+                                <li class="list-unstyled-item list-hours-item d-flex">
+                                    Thursday
+                                    <span class="ms-auto">7:00 AM to 8:00 PM</span>
+                                </li>
+                                <li class="list-unstyled-item list-hours-item d-flex">
+                                    Friday
+                                    <span class="ms-auto">7:00 AM to 8:00 PM</span>
+                                </li>
+                                <li class="list-unstyled-item list-hours-item d-flex">
+                                    Saturday
+                                    <span class="ms-auto">9:00 AM to 5:00 PM</span>
+                                </li>
+                            </ul>
+                            <p class="address mb-5">
+                                <em>
+                                    <strong>MT.Shop</strong>
+                                    <br />
+                                    Sumedang, Jawa Barat
+                                </em>
+                            </p>
+                            <p class="mb-0">
+                                <br />
+                                <a class="but btn btn-lg fw-bold" href="https://api.whatsapp.com/send?6282128391139&text=Saya%20tertarik%20dengan%20produk%20dari%20MT.Shop">
+                                <font color="black">
+                                Temui Kami Di Whatsapp
+                                </font>
+</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="page-section cta">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-9 mx-auto">
+                    <div class="cta-inner bg-faded text-center rounded">
+                            <h2 class="section-heading mb-4">
+                                  <font color="black">    
+                                <span class="section-heading-upper">Dari Kami</span>
+                                <span class="section-heading-lower">Untuk Kamu</span>
+                            </h2>
+                            <p class="mb-0">Jika Anda tidak puas, beri tahu kami dan kami akan melakukan apa pun yang kami bisa untuk memperbaiki keadaan!</p>
+</font>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <footer class="footer text-faded text-center py-5">
+            <div class="container"><p class="m-0 small">Copyright &copy; MT.Shop</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
